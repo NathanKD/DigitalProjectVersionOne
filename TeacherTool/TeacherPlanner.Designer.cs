a082177abb78509b489ae6d51e2a8d413117f173
NathanKD <111681499+NathanKD@users.noreply.github.com>
2023-04-05 12:25:44 +1200

    aaaa

---
+++ TeacherTool/TeacherPlanner.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TeacherTool
+{
+    partial class TeacherPlanner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Classroom = new System.Windows.Forms.SplitContainer();
+            this.studentSelect = new System.Windows.Forms.FlowLayoutPanel();
+            this.errorList = new System.Windows.Forms.ListView();
+            this.taskList = new System.Windows.Forms.ListView();
+            this.studentName = new System.Windows.Forms.Label();
+            this.studentWindow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Classroom)).BeginInit();
+            this.Classroom.Panel1.SuspendLayout();
+            this.Classroom.Panel2.SuspendLayout();
+            this.Classroom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.studentWindow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Classroom
+            // 
+            this.Classroom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Classroom.Location = new System.Drawing.Point(0, 0);
+            this.Classroom.Name = "Classroom";
+            // 
+            // Classroom.Panel1
+            // 
+            this.Classroom.Panel1.Controls.Add(this.studentSelect);
+            // 
+            // Classroom.Panel2
+            // 
+            this.Classroom.Panel2.Controls.Add(this.errorList);
+            this.Classroom.Panel2.Controls.Add(this.taskList);
+            this.Classroom.Panel2.Controls.Add(this.studentName);
+            this.Classroom.Panel2.Controls.Add(this.studentWindow);
+            this.Classroom.Size = new System.Drawing.Size(800, 450);
+            this.Classroom.SplitterDistance = 400;
+            this.Classroom.TabIndex = 0;
+            // 
+            // studentSelect
+            // 
+            this.studentSelect.AutoScroll = true;
+            this.studentSelect.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.studentSelect.Location = new System.Drawing.Point(0, 0);
+            this.studentSelect.Name = "studentSelect";
+            this.studentSelect.Size = new System.Drawing.Size(400, 450);
+            this.studentSelect.TabIndex = 0;
+            // 
+            // errorList
+            // 
+            this.errorList.HideSelection = false;
+            this.errorList.Location = new System.Drawing.Point(15, 217);
+            this.errorList.Name = "errorList";
+            this.errorList.Size = new System.Drawing.Size(242, 221);
+            this.errorList.TabIndex = 2;
+            this.errorList.UseCompatibleStateImageBehavior = false;
+            // 
+            // taskList
+            // 
+            this.taskList.HideSelection = false;
+            this.taskList.Location = new System.Drawing.Point(263, 217);
+            this.taskList.Name = "taskList";
+            this.taskList.Size = new System.Drawing.Size(121, 221);
+            this.taskList.TabIndex = 0;
+            this.taskList.UseCompatibleStateImageBehavior = false;
+            // 
+            // studentName
+            // 
+            this.studentName.AutoSize = true;
+            this.studentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentName.Location = new System.Drawing.Point(10, 9);
+            this.studentName.Name = "studentName";
+            this.studentName.Size = new System.Drawing.Size(154, 25);
+            this.studentName.TabIndex = 1;
+            this.studentName.Text = "Student_Name";
+            // 
+            // studentWindow
+            // 
+            this.studentWindow.Location = new System.Drawing.Point(13, 37);
+            this.studentWindow.Name = "studentWindow";
+            this.studentWindow.Size = new System.Drawing.Size(371, 174);
+            this.studentWindow.TabIndex = 0;
+            this.studentWindow.TabStop = false;
+            // 
+            // TeacherPlanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Classroom);
+            this.Name = "TeacherPlanner";
+            this.Load += new System.EventHandler(this.TeacherPlanner_Load);
+            this.Classroom.Panel1.ResumeLayout(false);
+            this.Classroom.Panel2.ResumeLayout(false);
+            this.Classroom.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Classroom)).EndInit();
+            this.Classroom.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.studentWindow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer Classroom;
+        private System.Windows.Forms.ListView errorList;
+        private System.Windows.Forms.ListView taskList;
+        private System.Windows.Forms.Label studentName;
+        private System.Windows.Forms.PictureBox studentWindow;
+        private System.Windows.Forms.FlowLayoutPanel studentSelect;
+    }
+}
+
---
+++ TeacherTool/TeacherPlanner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeacherTool
+{
+    public partial class TeacherPlanner : Form
+    {
+        public TeacherPlanner()
+        {
+            InitializeComponent();
+            Bitmap b = new Bitmap(100,100);
+            for (int x = 0; x < 100; x++) 
+                for(int y = 0; y < 100; y++)
+                    b.SetPixel(x, y, Color.Red);
+            
+            for(int i = 0; i < 100; i++)
+            {
+                StudentUIPanel test = new StudentUIPanel("Test_User", b);
+                studentSelect.Controls.Add(test);
+                test.profilePicture.Click += studentClick;
+                //test.BackColor = Color.Red;
+                test.setLocation(new Point(80 * i, 0));
+            }
+            
+            
+        }
+        
+        private void studentClick(object sender, EventArgs e)
+        {
+            PictureBox selected = (PictureBox)sender;
+            Console.WriteLine("Guh");
+        }
+        private void TeacherPlanner_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class StudentUIPanel : Panel
+    {
+        public PictureBox profilePicture;
+        public Label studentName;
+        public StudentUIPanel(string username, Bitmap profileBitmap)
+        {
+            studentName = new Label();
+            studentName.Text = username;
+            studentName.Parent = this;
+            studentName.Anchor = AnchorStyles.Bottom;
+            studentName.Location = new Point(50, 85);
+            studentName.TextAlign = ContentAlignment.TopCenter;
+
+            profilePicture = new PictureBox();
+            profilePicture.Image = profileBitmap;
+            profilePicture.Size = new Size(70,70);
+            profilePicture.Parent = this;
+            profilePicture.Dock = DockStyle.Fill;
+
+            this.Size = new Size(70,70);
+            this.BorderStyle = BorderStyle.FixedSingle;
+            this.Controls.Add(studentName);
+            this.Controls.Add(profilePicture);
+        }
+        public void setLocation(Point newLocation)
+        {
+            this.Location = newLocation;
+        }
+    }
+}
